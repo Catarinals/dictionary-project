99e285332154e79e00254c1a40af4a07be93c8e7
Catarinals <132306244+Catarinals@users.noreply.github.com>
2023-08-28 14:19:46 +0100

    I've hidden empty examples and synonyms

--- src/Example.js
+++ src/Example.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 export default function Example(props) {
-  if (props.example) {
+  if (props.example && props.example.length > 0) {
     return <span className="example">{props.example}</span>;
   } else {
     return null;
--- src/Meaning.js
+++ src/Meaning.js
@@ -16,20 +16,24 @@ export default function Meaning(props) {
               </span>
               {definition.definition}
             </div>
-            <div className="example">
-              <em>Example: </em>
-              <span className="example">
-                <Example example={definition.example} />
-              </span>
-            </div>
+            {definition.example && (
+              <div className="example">
+                <em>Example: </em>
+                <span className="example">
+                  <Example example={definition.example} />
+                </span>
+              </div>
+            )}
           </div>
         );
       })}
 
-      <div className="synonyms">
-        <span>Synonyms:</span>
-        <Synonyms synonyms={props.meaning.synonyms} />
-      </div>
+      {props.meaning.synonyms.length > 0 && (
+        <div className="synonyms">
+          <span>Synonyms:</span>
+          <Synonyms synonyms={props.meaning.synonyms} />
+        </div>
+      )}
     </div>
   );
 }
